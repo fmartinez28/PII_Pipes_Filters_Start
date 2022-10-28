9106b1b7ed086c8f9e036bfe9196fbb739f7901a
Facundo Martínez <facusml16@gmail.com>
2022-10-27 22:21:59 -0300

    Implementación completa?

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -53,9 +53,9 @@ namespace CompAndDel
         public static void Ejercicio4(string path){
             TwitterImage publisher = new();
             PictureProvider provider = new();
-            string imgPath = @".\luke.jpg"
+            string imgPath = @".\luke.jpg";
             IPicture loadedPic = provider.GetPicture(imgPath);
-            List<IPicture> outputImgs = LoadPipesAndFilters_Ejercicio4(loadedPic, imgPath);
+            List<IPicture> outputImgs = LoadPipesAndFilters_Ejercicio4(loadedPic);
             int sequenceCounter = 1;
             foreach (IPicture pic in outputImgs)                 //Itero y actúo sobre cada transformación de la secuencia
             {
@@ -78,9 +78,9 @@ namespace CompAndDel
 
         // Proceso
 
-        IPicture result = firstSerialPipe.Send(picture);
-        result = secondSerialPipe.Send(result);
-        result = nullPipe.Send(result);
+        IPicture result = nullPipe.Send(picture);
+        result = secondSerialPipe.Send(picture);
+        result = firstSerialPipe.Send(picture);
 
         return result;
         }
@@ -97,17 +97,17 @@ namespace CompAndDel
         PipeSerial firstSerialPipe = new(greyscaleFilter, secondSerialPipe);
 
         // Proceso
+        IPicture result = nullPipe.Send(picture);
+        imgSequence.Add(result);
+        result = secondSerialPipe.Send(picture);
+        imgSequence.Add(result);
+        result = firstSerialPipe.Send(picture);
+        imgSequence.Add(result);
 
-        IPicture result = firstSerialPipe.Send(picture);
-        imgSequence.Add(result);
-        result = secondSerialPipe.Send(result);
-        imgSequence.Add(result);
-        result = nullPipe.Send(result);
-        imgSequence.Add(result);
 
         return imgSequence;
         }
-        public static List<IPicture> LoadPipesAndFilters_Ejercicio4(IPicture picture, string path)
+        public static List<IPicture> LoadPipesAndFilters_Ejercicio4(IPicture picture)
         {
         
         List<IPicture> imgSequence = new();     //Secuencia de transformaciones de picture
@@ -117,7 +117,7 @@ namespace CompAndDel
             //If true
         
         PipeNull finalTrue = new();
-        TwitterFilter publish = new();          //FIXME: Crear un filtro si es necesario
+        TwitterFilter publish = new();          //FIXME: Crear un filtro si es necesario///FIXED
         PipeSerial serialTrue = new(publish, finalTrue);
 
             //If false
@@ -134,18 +134,26 @@ namespace CompAndDel
         PipeSerial start = new(greyFilter, conditionalPipe);
 
         // Proceso
+        
+        conditionalPipe.Evaluate(picture);
 
-        conditionalPipe.Evaluate(path);
-        IPicture result = start.Send(picture);
-        imgSequence.Add(result);
-        result = conditionalPipe.Send(result);
-        imgSequence.Add(result);
+        IPicture result;
 
-        PipeNull nextPipe = conditionalPipe.Next;
+        if (conditionalPipe.Evaluation){                    //Se podría mejorar
+            result = finalTrue.Send(picture);
+            imgSequence.Add(result);
+            result = serialTrue.Send(picture);
+            imgSequence.Add(result);
+        } else {
+            result = finalFalse.Send(picture);
+            imgSequence.Add(result);
+            result = serialFalse.Send(picture);
+            imgSequence.Add(result);
+        }
 
-        result = nextPipe.Send(result);
+        result = conditionalPipe.Send(picture);
         imgSequence.Add(result);
-        result = nextPipe.Next.Send(result);
+        result = start.Send(picture);
         imgSequence.Add(result);
 
         return imgSequence;
